810270649c8a1da3ec29d961352edeba9e32e942
Ankita Kuamri <ankitakumari2016@gmail.com>
2023-11-03 23:32:42 +0530

    java

--- src/TicTacToe.java
+++ src/TicTacToe.java
@@ -1,14 +1,19 @@
 import java.util.Scanner;
 
 public class TicTacToe {
-    private char[][] board;
-    private char currentPlayer;
 
-    public TicTacToe() {
+    private static char[][] board;
+
+    private static char currentPlayer = 'X';
+
+    public static void main(String[] args) {
+        initializeBoard();
+        printBoard();
+        playGame();
+    }
+
+    private static void initializeBoard() {
         board = new char[3][3];
-        currentPlayer = 'X';
-
-        // Initialize the board with empty spaces
         for (int i = 0; i < 3; i++) {
             for (int j = 0; j < 3; j++) {
                 board[i][j] = ' ';
@@ -16,113 +21,95 @@ public class TicTacToe {
         }
     }
 
-    public void printBoard() {
-        System.out.println("-------------");
+    private static void printBoard() {
+        System.out.println("  0 1 2");
         for (int i = 0; i < 3; i++) {
-            System.out.print("| ");
+            System.out.print(i + " ");
             for (int j = 0; j < 3; j++) {
-                System.out.print(board[i][j] + " | ");
+                System.out.print(board[i][j]);
+                if (j < 2) {
+                    System.out.print("|");
+                }
             }
             System.out.println();
-            System.out.println("-------------");
+            if (i < 2) {
+                System.out.println("  -----");
+            }
         }
     }
 
-    public boolean makeMove(int row, int col) {
+    private static void playGame() {
+        Scanner scanner = new Scanner(System.in);
+        int row=-1, col=-1;
+
+        do {
+            try {
+                System.out.println("Player " + currentPlayer + ", enter your move (row and column): ");
+                row = scanner.nextInt();
+                col = scanner.nextInt();
+            } catch (java.util.InputMismatchException e) {
+                System.out.println("Invalid input. Please enter valid integers for row and column.");
+                scanner.nextLine(); // Clear the invalid input
+                continue;
+            }
+        } while (!isValidMove(row, col));
+
+        board[row][col] = currentPlayer;
+        printBoard();
+
+        if (checkWin()) {
+            System.out.println("Player " + currentPlayer + " wins!");
+        } else if (isBoardFull()) {
+            System.out.println("It's a draw!");
+        } else {
+            currentPlayer = (currentPlayer == 'X') ? 'O' : 'X';
+            playGame();
+        }
+    }
+
+    private static boolean isValidMove(int row, int col) {
         if (row < 0 || row >= 3 || col < 0 || col >= 3 || board[row][col] != ' ') {
             System.out.println("Invalid move. Try again.");
             return false;
         }
-
-        board[row][col] = currentPlayer;
-        switchPlayer();
         return true;
     }
 
-    public boolean checkWin() {
-        // Check rows, columns, and diagonals for a win
-        for (int i = 0; i < 3; i++) {
-            if (board[i][0] != ' ' && board[i][0] == board[i][1] && board[i][1] == board[i][2]) {
-                return true; // Row win
-            }
-            if (board[0][i] != ' ' && board[0][i] == board[1][i] && board[1][i] == board[2][i]) {
-                return true; // Column win
-            }
-        }
-
-        if (board[0][0] != ' ' && board[0][0] == board[1][1] && board[1][1] == board[2][2]) {
-            return true; // Diagonal win
-        }
-        if (board[0][2] != ' ' && board[0][2] == board[1][1] && board[1][1] == board[2][0]) {
-            return true; // Diagonal win
-        }
-
-        return false; // No win
+    private static boolean checkWin() {
+        return (checkRows() || checkColumns() || checkDiagonals());
     }
 
-    public boolean isBoardFull() {
-        // Check if the board is full
+    private static boolean checkRows() {
+        for (int i = 0; i < 3; i++) {
+            if (board[i][0] != ' ' && board[i][0] == board[i][1] && board[i][1] == board[i][2]) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private static boolean checkColumns() {
+        for (int i = 0; i < 3; i++) {
+            if (board[0][i] != ' ' && board[0][i] == board[1][i] && board[1][i] == board[2][i]) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private static boolean checkDiagonals() {
+        return (board[0][0] != ' ' && board[0][0] == board[1][1] && board[1][1] == board[2][2]) ||
+                (board[0][2] != ' ' && board[0][2] == board[1][1] && board[1][1] == board[2][0]);
+    }
+
+    private static boolean isBoardFull() {
         for (int i = 0; i < 3; i++) {
             for (int j = 0; j < 3; j++) {
                 if (board[i][j] == ' ') {
-                    return false; // Board is not full
+                    return false;
                 }
             }
         }
-        return true; // Board is full
-    }
-
-    private void switchPlayer() {
-        currentPlayer = (currentPlayer == 'X') ? 'O' : 'X';
-    }
-
-    public static void main(String[] args) {
-        TicTacToe game = new TicTacToe();
-        Scanner scanner = new Scanner(System.in);
-
-        while (!game.checkWin() && !game.isBoardFull()) {
-            game.printBoard();
-
-            System.out.println("Player " + game.currentPlayer + "'s turn.");
-
-            int row = -1;
-            int col = -1;
-
-            boolean validInput = false;
-
-            while (!validInput) {
-                try {
-                    System.out.print("Enter row (0-2): ");
-                    String input = scanner.next();
-                    row = Integer.parseInt(input);
-
-                    System.out.print("Enter column (0-2): ");
-                    input = scanner.next();
-                    col = Integer.parseInt(input);
-
-                    if (row >= 0 && row < 3 && col >= 0 && col < 3 && game.board[row][col] == ' ') {
-                        validInput = true;
-                    } else {
-                        System.out.println("Invalid input. Please enter a valid position.");
-                    }
-                } catch (NumberFormatException e) {
-                    System.out.println("Invalid input. Please enter a valid integer.");
-                }
-            }
-
-            game.makeMove(row, col);
-        }
-
-        game.printBoard();
-
-        // Move the switchPlayer call here, after checking for a win
-        if (game.checkWin()) {
-            game.switchPlayer(); // Move this line here
-            System.out.println("Player " + game.currentPlayer + " wins!");
-        } else {
-            System.out.println("It's a draw!");
-        }
-
-        scanner.close();
+        return true;
     }
 }
